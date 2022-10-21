4e8aea3e6610c94b668eed32edc3510c41ad4621
luis <luisdi.28@estudiantec.cr>
2022-10-20 21:48:05 -0600

    Commit inicial en el que se buscaron las imágenes, se creo el main y se tenían ideas para implementar avances a futuro

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/Application.h"
+
+//Método que inicia la aplicación
+int main(int argc, char* argv[])
+{
+    Application application;
+
+    return application.startApplication();
+}
